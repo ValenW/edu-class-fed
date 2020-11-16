83b8590f0d9e6696462852194932b3e7dda3b7ee
Valen <wang_wenl@worksap.co.jp>
2020-11-16 17:37:35 +0800

    implement basic layout

---
+++ src/layout/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main> <router-view /> </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class index extends Vue {}
+</script>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,86 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Router, { RouteConfig } from 'vue-router'
+import Layout from '@/layout/index.vue'
 
 Vue.use(Router)
 
+const routes: RouteConfig[] = [
+  {
+    name: 'login',
+    path: '/login',
+    component: () =>
+      import(/* webpackChunkName: 'login' */ '@/views/login/index.vue')
+  },
+  {
+    path: '/',
+    component: Layout,
+    children: [
+      {
+        path: '/',
+        redirect: 'home'
+      },
+      {
+        name: 'home',
+        path: '/home',
+        component: () =>
+          import(/* webpackChunkName: 'home' */ '@/views/home/index.vue')
+      },
+      {
+        name: 'advert',
+        path: '/advert',
+        component: () =>
+          import(/* webpackChunkName: 'advert' */ '@/views/advert/index.vue')
+      },
+      {
+        name: 'advert-space',
+        path: '/advert-space',
+        component: () =>
+          import(
+            /* webpackChunkName: 'advert-space' */ '@/views/advert-space/index.vue'
+          )
+      },
+      {
+        name: 'course',
+        path: '/course',
+        component: () =>
+          import(/* webpackChunkName: 'course' */ '@/views/course/index.vue')
+      },
+      {
+        name: 'menu',
+        path: '/menu',
+        component: () =>
+          import(/* webpackChunkName: 'menu' */ '@/views/menu/index.vue')
+      },
+      {
+        name: 'resource',
+        path: '/resource',
+        component: () =>
+          import(
+            /* webpackChunkName: 'resource' */ '@/views/resource/index.vue'
+          )
+      },
+      {
+        name: 'role',
+        path: '/role',
+        component: () =>
+          import(/* webpackChunkName: 'role' */ '@/views/role/index.vue')
+      },
+      {
+        name: 'user',
+        path: '/user',
+        component: () =>
+          import(/* webpackChunkName: 'user' */ '@/views/user/index.vue')
+      }
+    ]
+  },
+  {
+    name: '404',
+    path: '*',
+    component: () =>
+      import(/* webpackChunkName: '404' */ '@/views/error-page/404.vue')
+  }
+]
+
 export default new Router({
-  routes: [
-  ]
+  routes
 })
