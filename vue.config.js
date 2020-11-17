066432f2117bff5717e1cccc4e7a6c020e61856b
Valen <wang_wenl@worksap.co.jp>
2020-11-17 16:48:33 +0800

    add devServer for proxy for api

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  devServer: {
+    proxy: {
+      '/boss': {
+        target: 'http://eduboss.lagou.com',
+        changeOrigin: true // 把请求头中的 host 配置为 target
+      },
+      '/front': {
+        target: 'http://edufront.lagou.com',
+        changeOrigin: true
+      }
+    }
+  },
   css: {
     loaderOptions: {
       scss: {
